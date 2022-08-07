5370d1082148e76b8a1d306547a6970b3e8afbba
黃柏浩 <howwoh1996@gmail.com>
2022-08-08 02:13:49 +0800

    202208080213

--- js/data.js
+++ js/data.js
@@ -34,5 +34,42 @@ page_data =[
         title : "實現輪播功能-小動畫呈現",
         pageURL : "006.html",
         tags :["Java Script","初學者"]
+    },
+    {
+        pageID : "007",
+        title : "Eclipse設定  編碼 自動填字 字體大小 背景色",
+        pageURL : "007.html",
+        tags :["Java","Eclipse"]
+    },
+    {
+        pageID : "008",
+        title : "JAVA 連接MYSQL",
+        pageURL : "008.html",
+        tags :["Java","MySQL"]
+    },
+    {
+        pageID : "009",
+        title : "sql語法：inner join, group by",
+        pageURL : "009.html",
+        tags :["MySQL","初學者"]
+    },
+    {
+        pageID : "010",
+        title : "DAO 運作模式 與MODEL(java bean)",
+        pageURL : "010.html",
+        tags :["Java",""]
+    },
+    {
+        pageID : "011",
+        title : "MYSQL 安裝",
+        pageURL : "011.html",
+        tags :["MySQL",""]
+    },
+    {
+        pageID : "012",
+        title : "VS code 實用小技巧(HTML)",
+        pageURL : "012.html",
+        tags :["HTML","VS Code"]
     }
+    
 ]
---
+++ md/react.md
@@ -0,0 +1,22 @@
+# react
+## 重要規則
+- 引入檔案有先後順序 (一定要先本體 在渲染)
+  - 1.react 本體
+  - 2.react 渲染
+  - 3.bebal 轉譯
+  
+- jsx 介紹
+js 擴充語法 可以用類似XML的方式存取
+自定義虛擬DOM 不能 加引號(因為不是當字串使用)
+實際寫起來 就像一般的HTML語法  
+要轉成 JS的內容(只能是表達式) 加入大括弧{}
+數值組的寫法 會有雙重大括弧 一層表示 是數值組(key:value) 一層表示是用JS語法
+寫JSX 語法時 script的type屬性要是 type="text/babel"
+才會以JSX來讀
+虛擬DOM只能有一個根標籤
+
+- react 渲染
+ReactDOM.render(虛擬DOM,要丟入的容器)
+虛擬DOM 是在JSX用 const 宣告的
+要丟入的容器 取得方法:document.getElementById('myId')
+
